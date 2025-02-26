bafeb9e279591706228971f9586bf8862f6c2e9e
Sanya Bochoun <76147037+sanya-bochoun@users.noreply.github.com>
2025-02-26 14:56:54 +0700

    Assignment : JavaScript Conditional Logic Assignment completed

--- ex-3.js
+++ ex-3.js
@@ -1,5 +1,10 @@
 //Exercise 3
 // Ternary Operator
-let lightBulbStatus = "Off";
+let lightBulbStatus = "On1";
 
 //Start coding here
+lightBulbStatus === "On"
+     ? console.log("Light bulb is on.")
+     : lightBulbStatus === "Off"
+          ? console.log("Light bulb is off.")
+          : console.log("Please choose the correct input (On/Off)");
--- ex-4.js
+++ ex-4.js
@@ -1,5 +1,18 @@
 //Exercise 4
 // Switch Statement
-let lightBulbStatus = "On";
+let lightBulbStatus = "On"
 
 //Start coding here
+switch (lightBulbStatus) {
+      case "On":
+        console.log("Light bulb is on");
+        break;
+      case "Off":
+        console.log("Light bulb is off");
+        break;
+      case "Broken":
+          console.log("Light bulb is broken");
+        break;  
+      default:
+        console.log("Please choose the correct input (On/Off/Broken)");
+};
